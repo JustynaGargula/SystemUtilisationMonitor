0b7f78bdf30f48106c160fa8a91c6cab5b24be3b
JustynaGargula <s221391@student.uek.krakow.pl>
2024-01-02 16:13:11 +0100

    Implemented Memory and CPU classes

--- CPU.py
+++ CPU.py
@@ -1,3 +1,44 @@
 import psutil
 
-print(psutil.cpu_times())
+
+class CPU:
+    def __init__(self):
+        self.user_time = 0
+        self.system_time = 0
+        self.idle_time = 0
+        self.current_freq = 0
+        self.max_freq = 0
+        self.percentage = 0
+
+        self.update_data()
+
+    def update_data(self):
+        cpu_times = psutil.cpu_times()
+        cpu_frequency = psutil.cpu_freq()
+
+        self.user_time = round(cpu_times[0]/60, 2)      # converted into minutes
+        self.system_time = round(cpu_times[1]/60, 2)
+        self.idle_time = round(cpu_times[2]/60, 2)
+
+        self.current_freq = cpu_frequency[0]/1000
+        self.max_freq = cpu_frequency[2]/1000
+
+        self.percentage = psutil.cpu_percent()      # has value 0
+        # self.percentage = psutil.cpu_percent(interval=1)
+
+    def show(self):
+        print("current frequency = " + str(self.current_freq) + " Ghz")
+        print("max frequency = " + str(self.max_freq) + " Ghz")
+        self.percentage = psutil.cpu_percent(interval=1)
+        print("CPU utilization percentage:  " + str(self.percentage) + " %")
+        print("normal processes time: " + str(self.user_time) + " minutes")
+        print("kernel mode processes time: " + str(self.system_time) + " minutes")
+        print("CPU doing nothing time: " + str(self.idle_time) + " minutes")
+
+
+# delete when main is done
+
+# print(psutil.cpu_times())
+# print(psutil.cpu_freq())
+cpu = CPU()
+cpu.show()
--- Memory.py
+++ Memory.py
@@ -1,3 +1,35 @@
 import psutil
 
-print(psutil.virtual_memory())
+
+class Memory:
+    def __init__(self):
+        self.total = 0   # in bytes     # without swap memory
+        self.total_GB = 0
+
+        self.available = 0     # in bytes
+        self.available_GB = 0
+
+        self.percent = 0  # usage percentage
+
+        self.update_data()
+
+    def update_data(self):
+        mem_parameters = psutil.virtual_memory()
+
+        self.total = mem_parameters[0]   # in bytes
+        self.total_GB = round(self.total / (1024 * 1024 * 1024), 2)
+
+        self.available = mem_parameters[1]     # in bytes
+        self.available_GB = round(self.available / (1024 * 1024 * 1024), 2)
+
+        self.percent = mem_parameters[2]  # usage percentage
+
+    def show_parameters(self):
+        print("total memory = " + str(self.total_GB) + " GB")
+        print("available memory = " + str(self.available_GB) + " GB")
+        print("used memory: " + str(self.percent) + " %")
+
+
+# delete when main is done
+m = Memory()
+m.show_parameters()
